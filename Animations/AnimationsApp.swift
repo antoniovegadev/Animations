bbce4e0de95fb31530cd47b9c8ac1cdb216d48a3
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-22 17:06:01 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Antonio Vega on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
